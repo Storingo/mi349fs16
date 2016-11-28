eb99e1ef4b7fd6ced4f809b00b81dac7499de009
Storingo <storingo@msu.edu>
2016-11-27 22:43:51 -0500

    Add files via upload

---
+++ Final/js/index.js
@@ -0,0 +1,12 @@
+function msg1() {
+  //alert("SUBMITTED");
+  document.getElementById("emailIn").value = "Submitted";
+}
+function msg2() {
+  //alert("SUBMITTED");
+  document.getElementById("about").value = "Thank you for your interest we'll look over your info";
+}
+function msg3(elem) {
+  //alert("SUBMITTED");
+  document.getElementById("about").value = "Thanks for the Feedback";
+}
